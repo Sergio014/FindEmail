f112dbf075e2ba8556828f83108f42314d1c66b1
Serhiy Dorosh <dorossergij13@gmail.com>
2023-06-16 11:03:40 +0300

    add TelegramUser model to admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-# register user model in admin panel
+# register models in admin panel
 admin.site.register(User)
+
+admin.site.register(TelegramUser)
