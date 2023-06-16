120898ed99fc36f342318b8001608988c2a0b405
Serhiy Dorosh <dorossergij13@gmail.com>
2023-06-16 19:33:07 +0300

    add subprocess

--- api/models.py
+++ api/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from telegram_bot.management.commands.bot import bot
-
+import subprocess
+import signal
 
 # Create your models here.
 class Bot(models.Model):
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
+        process = None
         if self.is_active:
-            bot.infinity_polling()
+            process = subprocess.Popen(["python", "/home/FindEmail/target_directory/FindEmail/manage.py", "bot"])
+        elif process is not None:
+            process.kill()
         else:
-            bot.stop_bot()
+            print(process)
         super().save(*args, **kwargs)
