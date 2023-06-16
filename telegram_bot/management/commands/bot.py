4649049e3047ef0acd3c503a0ec38038b172a636
Serhiy Dorosh <dorossergij13@gmail.com>
2023-06-16 13:50:17 +0300

    create function to check is bot active

--- telegram_bot/management/commands/bot.py
+++ telegram_bot/management/commands/bot.py
@@ -3,56 +3,60 @@ import requests
 from django.core.management.base import BaseCommand
 from telebot import types
 from .token import TOKEN
-# initialize bot using it token
+
 bot_token = TOKEN
 bot = telebot.TeleBot(bot_token)
 
-# some russian
-def webAppKeyboard(link): #создание клавиатуры с webapp кнопкой
-   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
-   webAppTest = types.WebAppInfo(link) #создаем webappinfo - формат хранения url
-   one_butt = types.KeyboardButton(text="Go to website", web_app=webAppTest) #создаем кнопку типа webapp
-   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
+def webAppKeyboard(link):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1)
+    webAppTest = types.WebAppInfo(link)
+    one_butt = types.KeyboardButton(text="Go to website", web_app=webAppTest)
+    keyboard.add(one_butt)
 
-   return keyboard
+    return keyboard
 
 def send_full_info_to_user(tel_id, data):
     bot.send_message(tel_id, data, reply_markup=types.ReplyKeyboardRemove(selective=True))
 
 user = {}
 
+# Function to check if the bot is active
+def is_bot_active():
+    response = requests.get('https://findemail.pythonanywhere.com/api-v1/check_bot').json()
+    return response['is_active']
 
-# if bot is inactive stop replaying to any messages
-response = requests.get('https://findemail.pythonanywhere.com/api-v1/check_bot').json()
-if not response['is_active']:
+# Handler for inactive bot
+if not is_bot_active:
     @bot.message_handler(func=lambda message: True)
     def handle_message_if_inactive(message):
-        pass
+        bot.reply_to(message, "Sorry, the bot is currently inactive.")
 
 @bot.message_handler(commands=['start'])
-# if user send /start command
 def start(message):
+    if not is_bot_active():
+        return
     bot.reply_to(message, "Hello! Please, provide your email")
-    bot.register_next_step_handler(message, check_email) # connect another handler to check response of user
+    bot.register_next_step_handler(message, check_email)
 
 def check_email(message):
-    # spam protection
     if message.text == '/start':
         return
-    # checking through API
+
+    if not is_bot_active():
+        return
+
     response = requests.get(f'https://findemail.pythonanywhere.com/api-v1/check/{message.text}/{message.chat.id}')
     response_dict = response.json()
-    # if all is OK
+
     if response.status_code == 200:
-        bot.reply_to(message, f'{response_dict["success"]}\n to see mo information please click button below and confirm your email', reply_markup=webAppKeyboard("https://findemail.pythonanywhere.com"))
+        bot.reply_to(message, f'{response_dict["success"]}\n to see more information, please click the button below and confirm your email', reply_markup=webAppKeyboard("https://findemail.pythonanywhere.com"))
     else:
         bot.reply_to(message, response_dict['error'])
 
 class Command(BaseCommand):
-    help = 'Implemented to Django application telegram bot setup command'
+    help = 'Implemented Django application telegram bot setup command'
 
     def handle(self, *args, **kwargs):
-            bot.enable_save_next_step_handlers()
-            bot.load_next_step_handlers()
-            bot.infinity_polling()
-
+        bot.enable_save_next_step_handlers()
+        bot.load_next_step_handlers()
+        bot.infinity_polling()
