dedb559c591536de2fbcba1dfa817fa4067291f5
Serhiy Dorosh <dorossergij13@gmail.com>
2023-06-16 18:33:44 +0300

    add polling control

--- api/models.py
+++ api/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from telegram_bot.management.commands.bot import bot
+
 
 # Create your models here.
 class Bot(models.Model):
     is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.is_active:
+            bot.infinity_polling()
+        else:
+            bot.stop_bot()
+        super().save(*args, **kwargs)
