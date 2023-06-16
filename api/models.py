53e2c31fc7d39041b04fce33a936ec40b0c6e35f
Serhiy Dorosh <dorossergij13@gmail.com>
2023-06-16 19:45:33 +0300

    one more small change

--- api/models.py
+++ api/models.py
@@ -3,16 +3,13 @@ from telegram_bot.management.commands.bot import bot
 import subprocess
 import signal
 
-process = None
+
 # Create your models here.
 class Bot(models.Model):
     is_active = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        if self.is_active:
-            process = subprocess.Popen(["python", "/home/FindEmail/target_directory/FindEmail/manage.py", "bot"])
-        elif process is not None:
+        process = subprocess.Popen(["python", "/home/FindEmail/target_directory/FindEmail/manage.py", "bot"])
+        if not self.is_active:
             process.kill()
-        else:
-            print(process)
         super().save(*args, **kwargs)
