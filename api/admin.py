a12f1d44875ebb47d48e11269ad9c4d9cf6cd5f3
Serhiy Dorosh <dorossergij13@gmail.com>
2023-06-26 09:08:48 +0300

    Starting monitoring through admin panel

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register bot model to manage telegram bot through admin panel
+admin.site.register(Monitoring)
+
 admin.site.register(Bot)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from telegram_bot.management.commands.bot import bot
+from telegram_bot.management.commands.monitoring import monitoring_job
 import subprocess
 import signal
 
@@ -12,4 +12,12 @@ class Bot(models.Model):
         process = subprocess.Popen(["python", "/home/FindEmail/target_directory/FindEmail/manage.py", "bot"])
         if not self.is_active:
             process.kill()
+        super().save(*args, **kwargs)
+
+class Monitoring(models.Model):
+    date = models.DateTimeField()
+    description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        monitoring_job()
         super().save(*args, **kwargs)
